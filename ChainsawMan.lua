8bd06badfdfe4abcea2b0f8640d2ec220ec5342e
desudoru <rf4jdwsymy@privaterelay.appleid.com>
2026-01-03 08:00:36 +0900

    Remove comments and update window title

--- ChainsawMan.lua
+++ ChainsawMan.lua
@@ -1,6 +1,3 @@
-// Variables used by Scriptable.
-// These must be at the very top of the file. Do not edit.
-// icon-color: deep-gray; icon-glyph: magic;
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
